4b941be528c1b4bd50e61d9bd6c2d30ad5146dd0
srconline <srcjnu@gmail.com>
2018-12-01 00:07:13 +0530

    commit

---
+++ vakut/src/main/java/com/nse/NseParserApplication.java
@@ -0,0 +1,12 @@
+package com.nse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NseParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NseParserApplication.class, args);
+	}
+}
